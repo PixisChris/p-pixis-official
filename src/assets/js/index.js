a71934eb727e42ef162d3ea031e7c27a86a55718
DopplerKuo <doppler.kuo@gmail.com>
2021-04-16 01:42:03 +0800

    style nav

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,1 +1,23 @@
-import './_helper'
+import './_helper';
+
+// TODO: utils.js
+const toggleClassName = (el, className) => {
+  document.querySelector(el).classList.toggle(className);
+};
+
+const docReady = (fn) => {
+  if (
+    document.readyState === 'complete' ||
+    document.readyState === 'interactive'
+  ) {
+    setTimeout(fn, 1);
+  } else {
+    document.addEventListener('DOMContentLoaded', fn);
+  }
+};
+
+docReady(() => {
+  document.querySelector('#nav .product-btn').addEventListener('click', () => {
+    toggleClassName('#nav', 'active');
+  });
+});
