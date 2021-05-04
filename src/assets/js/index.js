7416bd442955fda982ed765753accacd92ec4140
hungmi <hungmihungmi@gmail.com>
2021-05-04 17:41:36 +0800

    add chatbot script to all head

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -22,7 +22,6 @@ window.addEventListener('load', () => {
 docReady(() => {
   loadHTMLById('nav-components', 'nav.html');
   loadHTMLById('footer-components', 'footer.html');
-  loadHTMLById('chatbot-components', 'chatbot/script.html');
 
   document
     .querySelector('#float-banner .close-btn')
