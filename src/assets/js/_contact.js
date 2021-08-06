9658cf9071ffbbf5726ccecc70872ea41785cb60
hungmi <hungmihungmi@gmail.com>
2021-08-06 21:10:14 +0800

    翻譯警告訊息 刪除 AWS api 並註解 for 串接自家 DB

--- src/assets/js/_contact.js
+++ src/assets/js/_contact.js
@@ -1,37 +1,21 @@
-const helper = require('./helper')
-console.log(helper.ajax)
-
 document.addEventListener('DOMContentLoaded', function () {
 	if (document.body.classList.contains('js-contact-page')) {
 		document.querySelector('.js-contact-form').addEventListener('submit', function(e) {
             e.preventDefault();
             // console.log(e.target);
-            var params = {
-                FunctionName : 'sendPost',
-                InvocationType : 'RequestResponse',
-                LogType : 'None',
-                Payload: JSON.stringify( $('.js-contact-form').serializeArray() ),
-            };
-            invokeLambda(params);
+            // TODO 取得表單資料： JSON.stringify( $('.js-contact-form').serializeArray() )
+            invokeApi();
         })
-
-        AWS.config.update({region: 'us-east-1'});
-        AWS.config.credentials = new AWS.CognitoIdentityCredentials({IdentityPoolId: 'us-east-1:6c3d3a58-8ae6-4e33-83b0-45486af75258'});
-        
-        // Prepare to call Lambda function
-        var lambda = new AWS.Lambda({region: 'ap-northeast-2', apiVersion: '2015-03-31'});
 	}
 
-	function invokeLambda(params) {
-        // Call the Lambda function to collect the spin results
-        lambda.invoke(params, function(err, data) {
-            if (err) {
-                alert('很抱歉，為了帶給您最好的服務，我們正在努力更新系統中，請稍後再試。')
-            } else {
-                pullResults = JSON.parse(data.Payload);
-                location.href = 'contact-success.html';
-                console.log('lambda success');
-            }
-        });
+	function invokeApi() {
+        // TODO 請在此串接 DB
+        if (localStorage.getItem('pixis_lang') === 'tw') {
+            alert('很抱歉，為了帶給您最好的服務，我們正在努力更新系統中，請稍後再試。')
+        } else {
+            alert('Sorry, in order to bring you the best service, we are working hard to update the system, please try again later.')
+        }
+        // // TODO if success
+        // location.href = 'contact-success.html';
     }
 })
--- src/assets/js/_white-paper.js
+++ src/assets/js/_white-paper.js
@@ -1,46 +1,21 @@
-const helper = require('./helper')
-console.log(helper.ajax)
-
 document.addEventListener('DOMContentLoaded', function () {
-	if (document.body.classList.contains('js-white-paper-page')) {
-		document.querySelector('.js-download-form').addEventListener('submit', function(e) {
-			// 如果要直接串接自有後端 api START
-			// e.preventDefault()
-		 //    helper.ajax(e.target.getAttribute('method'), e.target.getAttribute('action'), new URLSearchParams(new FormData(e.target)).toString(), function(resp) {
-		 //        console.log(resp)
-		 //    }, function(resp) {
-		 //        console.log('fail')
-		 //    })
-		 // 如果要直接串接自有後端 api END
-
+    if (document.body.classList.contains('js-white-paper-page')) {
+        document.querySelector('.js-download-form').addEventListener('submit', function(e) {
             e.preventDefault();
             // console.log(e.target);
-            var params = {
-                FunctionName : 'sendPost',
-                InvocationType : 'RequestResponse',
-                LogType : 'None',
-                Payload: JSON.stringify( $('.js-download-form').serializeArray() ),
-            };
-            invokeLambda(params);
+            // TODO 取得表單資料： JSON.stringify( $('.js-contact-form').serializeArray() )
+            invokeApi();
         })
+    }
 
-        AWS.config.update({region: 'us-east-1'});
-        AWS.config.credentials = new AWS.CognitoIdentityCredentials({IdentityPoolId: 'us-east-1:6c3d3a58-8ae6-4e33-83b0-45486af75258'});
-        
-        // Prepare to call Lambda function
-        var lambda = new AWS.Lambda({region: 'ap-northeast-2', apiVersion: '2015-03-31'});
-	}
-
-	function invokeLambda(params) {
-        // Call the Lambda function to collect the spin results
-        lambda.invoke(params, function(err, data) {
-            if (err) {
-                alert('很抱歉，為了帶給您最好的服務，我們正在努力更新系統中，請稍後再試。')
-            } else {
-                pullResults = JSON.parse(data.Payload);
-                location.href = 'white-paper-success.html';
-                console.log('lambda success');
-            }
-        });
+    function invokeApi() {
+        // TODO 請在此串接 DB
+        if (localStorage.getItem('pixis_lang') === 'tw') {
+            alert('很抱歉，為了帶給您最好的服務，我們正在努力更新系統中，請稍後再試。')
+        } else {
+            alert('Sorry, in order to bring you the best service, we are working hard to update the system, please try again later.')
+        }
+        // // TODO if success
+        // location.href = 'contact-success.html';
     }
 })
